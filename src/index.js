311b2001e2bd9f4c7d0b3114a9244d19946540f5
aliflikescoding <alif.likes.coding@gmail.com>
2023-11-18 12:29:20 +0700

    Get basic API usage

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const getData = async () => {
+  try {
+    const response = await fetch(
+      "https://api.weatherapi.com/v1/current.json?key=94beffc60c1048cbabc81744231211&q=jakarta",
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+};
+getData();
