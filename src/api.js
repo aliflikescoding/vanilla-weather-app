09d6e755f2003bb1e2c03025d797a16f5ced81ed
aliflikescoding <alif.likes.coding@gmail.com>
2023-11-19 12:23:23 +0700

    Add loading

--- src/api.js
+++ src/api.js
@@ -9,8 +9,11 @@ const api = (() => {
         `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`,
         { mode: "cors" }
       );
+      const loading = document.querySelector(".loading");
+      loading.classList.remove("invis");
       const data = await response.json();
       dom.showCurrent(data);
+      loading.classList.add("invis");
     } catch (error) {
       console.error("Error fetching data", error);
     }
